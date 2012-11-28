49e4d92fec2564ff9c18a769dcc82f6da1b7bb5f
Brandon Hilkert <brandonhilkert@gmail.com>
2012-11-28 08:07:21 -0500

    Don't access redis from app.

--- lib/project/app.rb
+++ lib/project/app.rb
@@ -13,10 +13,6 @@ module Project
     end
 
     helpers do
-      def redis
-        Project.redis
-      end
-
       def asset_path(source)
         "/assets/" + settings.sprockets.find_asset(source).digest_path
       end
